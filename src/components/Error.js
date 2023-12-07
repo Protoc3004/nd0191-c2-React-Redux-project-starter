6062c6d2453a8937fb041a1153fb581b058f5eda
TuoiNguyenXuan <nguyentuoi.lachimolala@email.com>
2023-12-07 08:26:21 +0700

    commit code

---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="container">
+    <div className="row">
+      <h1>Error 404</h1>
+    </div>
+  </div>
+);
+
+export default NotFound;
